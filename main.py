b573145ecd9301dbeb8ea0a67439c94bf2d52c38
hector-cr <hcastanonruiz@gmail.com>
2025-04-11 16:17:54 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 def suma(a, b):
     return a + b
-def resta(a,b)
+def resta(a,b):
     return a-b
 def multiplicacion(a, b):
     return a * b
+def division (a,b):
+    return a/b if b!=0 else "Error: division por cero"
+
